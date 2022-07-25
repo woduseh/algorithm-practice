9b06774060b4b02ad45e138c0f54c4c25dcdb870
HWANG JAEYEON <woduseh@gmail.com>
2022-07-25 14:01:26 +0900

    Comment: 출력값에 대한 설명 주석 추가

--- src/main/java/practice/generic/CountSum.java
+++ src/main/java/practice/generic/CountSum.java
@@ -7,24 +7,32 @@ public class CountSum {
         CountSum countSum = new CountSum();
         final long THRESHOLD = 1_000_000_000L;
 
-//        long startTime = System.currentTimeMillis();
-//        System.out.println(countSum.iterativeSum(THRESHOLD));
-//        long endTime = System.currentTimeMillis();
-//
-//        System.out.println("Iterative sum time: " + (endTime - startTime) + "ms");
-//
-//        startTime = System.currentTimeMillis();
-//        System.out.println(countSum.streamSum(THRESHOLD));
-//        endTime = System.currentTimeMillis();
-//
-//        System.out.println("Stream sum time: " + (endTime - startTime) + "ms");
+        // Parallel vs iterative
+        long startTime = System.currentTimeMillis();
+        System.out.println(countSum.iterativeSum(THRESHOLD));
+        long endTime = System.currentTimeMillis();
 
+        System.out.println("Iterative sum time: " + (endTime - startTime) + "ms");
+
+        startTime = System.currentTimeMillis();
+        System.out.println(countSum.streamSum(THRESHOLD));
+        endTime = System.currentTimeMillis();
+
+        System.out.println("Stream sum time: " + (endTime - startTime) + "ms");
+
+        // Generic
         System.out.println("Generic Arithmetic Operation");
         System.out.println(countSum.add(10, 2.3));
         System.out.println(countSum.sub(10, 25L));
+
+        // Number 인터페이스릐 doubleValue() 를 이용하기에 오차가 발생할 수 있음
         System.out.println(countSum.mul(10, 2.3F));
+        System.out.println(countSum.mul(10, 2.3D));
+
         System.out.println(countSum.div(10, 5.5D));
-        System.out.println(countSum.div(new StringNumber("3.3"), new StringNumber(".1")));
+
+        // Number 인터페이스릐 doubleValue() 를 이용하기에 오차가 발생할 수 있음
+        System.out.println(countSum.div(new StringNumber("-3.3"), new StringNumber(".1")));
     }
 
     private long iterativeSum(long i) {
@@ -58,7 +66,7 @@ public class CountSum {
     static final class StringNumber extends Number {
         private final String value;
         public StringNumber(String value) {
-            if (value.matches("((\\+|-)?([0-9]+)(\\.[0-9]+)?)|((\\+|-)?\\.?[0-9]+)")) {
+            if (value.matches("(([+-])?(\\d+)(\\.\\d+)?)|(([+-])?\\.?\\d+)")) {
                 this.value = value;
             } else {
                 throw new IllegalArgumentException("Invalid value: not a number");
